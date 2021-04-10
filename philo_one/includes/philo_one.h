aa0021474eeb0df9168518df777610cc3fef9a3f
yeonhlee <yeonhlee@student.42seoul.kr>
2021-04-10 09:57:45 +0900

    add includes

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -3,16 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   philo_one.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeonhlee <yeonhlee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yeonhlee <yeonhlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 20:30:17 by yeonhlee          #+#    #+#             */
-/*   Updated: 2021/04/07 20:31:14 by yeonhlee         ###   ########.fr       */
+/*   Updated: 2021/04/10 09:56:55 by yeonhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_ONE_H
 # define PHILO_ONE_H
 
-# include <stdio.h>
+# include <string.h>	// memset
+# include <stdio.h>		// printf
+# include <stdlib.h>	// malloc, free
+# include <unistd.h>	// write, usleep
+# include <sys/time.h>	// gettimeofdayf
+# include <pthread.h>	// pthread_create, pthread_detach, pthread_join
+						// pthread_mutex_init, pthread_mutex_destroy
+						// pthread_mutex_lock, pthread_mutex_unlock
+
+# define KO			0
+# define OK			1
+
+/*
+** =============================================================================
+**	struct
+** =============================================================================
+*/
+
+typedef struct		s_data
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philosopher_must_eat;
+	
+}					t_data;
+
+/*
+** =============================================================================
+**	util.c
+** =============================================================================
+*/
+
+int					ft_atoi(const char *nptr);
 
 #endif
--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -3,25 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeonhlee <yeonhlee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yeonhlee <yeonhlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 20:33:53 by yeonhlee          #+#    #+#             */
-/*   Updated: 2021/04/07 20:35:33 by yeonhlee         ###   ########.fr       */
+/*   Updated: 2021/04/10 09:56:49 by yeonhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
 
-int		main(int argc, char **argv)
+int		ft_free(t_data *data, int result)
 {
+	if (!data)
+		free(data);
+	while (1)
+		;
+	return (result);
+}
+
+int			init_data(t_data *data, int argc, char **argv)
+{
+	int		i;
+
+	if (!(data = (t_data *)malloc(sizeof(data))))
+		return (KO);
+	i = ft_atoi(argv[1]);
+	data->number_of_philosophers = i;
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+	else
+		data->number_of_times_each_philosopher_must_eat = 0;
+	return (OK);
+}
+
+int			main(int argc, char **argv)
+{
+	t_data	*data;
+
+	data = NULL;
+	if (argc != 5 && argc != 6)
+		return (0);
+	if (!init_data(data, argc, argv))
+		return (ft_free(data, KO));
 	
-	
-	
-	
-	
-	
-	(void)argc;		//
-	(void)argv;		//
-	
-	return (0);
+	return (ft_free(data, OK));
 }
